7f1c346b5b68db5af29e9029650930791b336d7d
Connor Fowler <connorfowlerroth@gmail.com>
2022-08-02 01:06:27 -0400

    Get Products added

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -12,9 +12,30 @@ namespace GeneralStoreAPI.Controllers
     public class ProductController : Controller
     {
         private GeneralStoreDBContext _db;
-        public ProductController(GeneralStoreContext db) {
+        public ProductController(GeneralStoreContext db)
+        {
             _db = db;
-        }   
-        
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductEdit newProduct)
+        {
+            ProductController product = new ProductController()
+            {
+                Name = newProduct.Name,
+                Price = newProduct.price,
+                QuantityInStock = newProduct.Quantity,
+            };
+
+            _db.Products.Add(products);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts() {
+            var products = await _db.Products.ToListAsync();
+            return Ok(products);
+        }
     }
 }
