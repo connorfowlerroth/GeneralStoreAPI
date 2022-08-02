81c57476c8d045b56b713b9fd79044761f2ad634
Connor Fowler <connorfowlerroth@gmail.com>
2022-08-02 00:49:51 -0400

    Product Controller added

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeneralStoreAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeneralStoreAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private GeneralStoreDBContext _db;
+        public ProductController(GeneralStoreContext db) {
+            _db = db;
+        }   
+        
+    }
+}
